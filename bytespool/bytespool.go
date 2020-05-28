f7edcefea5a76b629887a6a4c275420da675c59d
wwq <2993895632@qq.com>
2020-05-28 18:01:01 +0800

    feat: add bytespool

--- bytespool/bytespool.go
+++ bytespool/bytespool.go
@@ -25,12 +25,12 @@ var (
 
 // Get Get
 func Get(size int) *bytes.Buffer {
-	if size < 1024 {
+	if size <= 1024 {
 		br := bytes1KPool.Get().(*bytes.Buffer)
 		br.Reset()
 		return br
 	}
-	if size < 4*1024 {
+	if size <= 4*1024 {
 		br := bytes4KPool.Get().(*bytes.Buffer)
 		br.Reset()
 		return br
@@ -38,5 +38,19 @@ func Get(size int) *bytes.Buffer {
 	br := bytes1MPool.Get().(*bytes.Buffer)
 	br.Reset()
 	return br
+}
 
+// Put Put
+func Put(br *bytes.Buffer) {
+	size := br.Cap()
+	if size <= 1024 {
+		bytes1KPool.Put(br)
+		return
+	}
+	if size <= 4*1024 {
+		bytes4KPool.Put(br)
+		return
+	}
+	bytes1MPool.Put(br)
+	return
 }
