8dd7c4fff6e03e1984638e00e08b137e1420f035
wwq <2993895632@qq.com>
2020-05-29 13:59:58 +0800

    feat: add subscribe impl

--- codec/message/unsub_ack.go
+++ codec/message/unsub_ack.go
@@ -3,7 +3,11 @@ package message
 import (
 	"bufio"
 
+	"github.com/wwq1988/mqtt/bytespool"
+	"github.com/wwq1988/mqtt/bytesutil"
 	"github.com/wwq1988/mqtt/codec/controltype"
+	"github.com/wwq1988/mqtt/codec/header"
+	"github.com/wwq1988/mqtt/util"
 )
 
 func init() {
@@ -13,6 +17,7 @@ func init() {
 // UnSubAck UnSubAck
 type UnSubAck struct {
 	flags uint8
+	MsgID uint16
 }
 
 // NewUnSubAck NewUnSubAck
@@ -24,10 +29,49 @@ func NewUnSubAck(flags uint8) Message {
 
 // Decode Decode
 func (m *UnSubAck) Decode(data []byte) error {
+	b := bytesutil.Get(data)
+	defer bytesutil.Put(b)
+	m.MsgID = b.ReadUint16()
 	return nil
 }
 
 // EncodeTo EncodeTo
 func (m *UnSubAck) EncodeTo(bw *bufio.Writer) error {
+	if err := m.writeHeader(bw); err != nil {
+		return err
+	}
+	if err := m.writeBody(bw); err != nil {
+		return err
+	}
 	return nil
+}
+
+func (m *UnSubAck) writeHeader(bw *bufio.Writer) error {
+	h := header.Get()
+	defer header.Put(h)
+	h.SetControlType(controltype.UnSubAck)
+	m.setFlags()
+	h.SetFlags(m.flags)
+	if err := h.EncodeTo(bw); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *UnSubAck) writeBody(bw *bufio.Writer) error {
+	dataLen := 2
+
+	bytesBuffer := bytespool.Get(int(dataLen))
+	defer bytespool.Put(bytesBuffer)
+
+	bytesBuffer.Write(util.Uint16ToBytes(m.MsgID))
+
+	if _, err := bw.Write(bytesBuffer.Bytes()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *UnSubAck) setFlags() {
+
 }
--- codec/message/unsubscribe.go
+++ codec/message/unsubscribe.go
@@ -3,7 +3,12 @@ package message
 import (
 	"bufio"
 
+	"github.com/wwq1988/mqtt/bytespool"
+	"github.com/wwq1988/mqtt/bytesutil"
 	"github.com/wwq1988/mqtt/codec/controltype"
+	"github.com/wwq1988/mqtt/codec/header"
+	"github.com/wwq1988/mqtt/codec/qos"
+	"github.com/wwq1988/mqtt/util"
 )
 
 func init() {
@@ -12,7 +17,9 @@ func init() {
 
 // UnSubscribe UnSubscribe
 type UnSubscribe struct {
-	flags uint8
+	flags  uint8
+	MsgID  uint16
+	Topics []*Topic
 }
 
 // NewUnSubscribe NewUnSubscribe
@@ -24,10 +31,67 @@ func NewUnSubscribe(flags uint8) Message {
 
 // Decode Decode
 func (m *UnSubscribe) Decode(data []byte) error {
+	b := bytesutil.Get(data)
+	defer bytesutil.Put(b)
+	m.MsgID = b.ReadUint16()
+	for !b.EOF() {
+		dataLen := b.ReadUint16()
+		name := b.ReadStr(dataLen)
+		qos := qos.Parse(b.ReadByte())
+		m.Topics = append(m.Topics, &Topic{
+			Name: name,
+			Qos:  qos,
+		})
+	}
 	return nil
 }
 
 // EncodeTo EncodeTo
 func (m *UnSubscribe) EncodeTo(bw *bufio.Writer) error {
+	if err := m.writeHeader(bw); err != nil {
+		return err
+	}
+	if err := m.writeBody(bw); err != nil {
+		return err
+	}
 	return nil
+}
+
+func (m *UnSubscribe) writeHeader(bw *bufio.Writer) error {
+	h := header.Get()
+	defer header.Put(h)
+	h.SetControlType(controltype.UnSubscribe)
+	m.setFlags()
+	h.SetFlags(m.flags)
+	if err := h.EncodeTo(bw); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *UnSubscribe) writeBody(bw *bufio.Writer) error {
+	dataLen := uint32(2)
+	for _, topic := range m.Topics {
+		dataLen += 2 + uint32(len(topic.Name)) + 1
+	}
+	bytesBuffer := bytespool.Get(int(dataLen))
+	defer bytespool.Put(bytesBuffer)
+
+	bytesBuffer.Write(util.Uint16ToBytes(m.MsgID))
+
+	for _, topic := range m.Topics {
+		dataLen += 2 + uint32(len(topic.Name)) + 1
+		bytesBuffer.Write(util.Uint16ToBytes(uint16(len(topic.Name))))
+		bytesBuffer.WriteString(topic.Name)
+		bytesBuffer.WriteByte(byte(topic.Qos))
+	}
+
+	if _, err := bw.Write(bytesBuffer.Bytes()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *UnSubscribe) setFlags() {
+
 }
